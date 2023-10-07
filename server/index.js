fe14157f730eaf2b5da400e2338ce849a9a38e4d
BREAKY MCQUEEN <breakymcqueen@s-MacBook-Pro.local>
2023-10-07 16:53:13 +0700

    First commit Message

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import path from 'path';
+import  express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+const port = process.env.port || 8000;
+import userRoutes from "./routes/userRoutes.js";
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'client/dist')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Server is ready'))
+}
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
